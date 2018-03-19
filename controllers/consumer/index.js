80229d9b6aaf0e1abe41f0ec0c7e114dccd63dbb
okawong <parker.okawong@gmail.com>
2018-03-18 23:57:52 -0400

    added requirement for pulling available messages seperately

--- controllers/consumer/index.js
+++ controllers/consumer/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../../db');
+var messages = require('../messages/message-controller.js');
 exports.engine = 'hbs';
+const AVAILABLE_STATE = 1;
+const CHECKED_OUT_STATE = 2;
+
 router.get('/', function(req, res, next){
   // res.render('consumer-index', {messages:db.messages});
-  res.render(__dirname+'\\views\\view', {messages:db.messages});
+  var availableData = getfilteredMessages(AVAILABLE_STATE);
+  var checkedoutData = getfilteredMessages(CHECKED_OUT_STATE);
+
+  res.render(__dirname+'\\views\\view', {messages:availableData, checkedoutMessages:checkedoutData});
 
 });
+
+function getfilteredMessages(state){
+  //done this way to pass by value, not reference
+  var filteredMessages = Object.assign({}, db.messages);
+  for(var id in filteredMessages){
+    if(filteredMessages[id].state != state){
+      delete filteredMessages[id];
+    }
+  }
+  return filteredMessages;
+}
 module.exports = router;
